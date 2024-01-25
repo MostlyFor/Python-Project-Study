9cb3d0c954fcf559c1c2d385313a09e8a9d1aa09
sseung_ye <112370282+seungye-kwak@users.noreply.github.com>
2024-01-25 09:05:29 +0900

    Create day_02.py
    project_02: 팁 계산기

---
+++ 곽승예/day_02.py
@@ -0,0 +1,8 @@
+# 2일차 프로젝트: 팁 계산기
+
+print("Welcome to the tip calculator.")
+bill = input("What was the total bill?")
+per = input("What percentage tip would you like to give? 10, 12, or 15?")
+people = input("How many people to split the bill?")
+each = round((float(bill[1:])*(1+int(per)/100))/int(people),2)
+print(f"Each person should pay: ${each}")
