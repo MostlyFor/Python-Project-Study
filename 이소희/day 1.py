f9d1766c275f7b3b2d574c6f2e831db158f6672f
soheeconpanna <157996502+soheeconpanna@users.noreply.github.com>
2024-01-29 06:57:02 +0900

    밴드명 생성기

--- 이소희/day 1.py
+++ 이소희/day 1.py
@@ -1,1 +1,14 @@
-#test
+#1. Create a greeting for your program.
+print("안녕하세요, 반갑습니다.")
+print("밴드 이름 작명기 서비스에 오신 걸 환영합니다.💞")
+print("밴드 이름을 지어드릴까요? Enter를 눌러주세요.\n")
+input()
+#2. Ask the user for the city that they grew up in.
+city = input("당신은 어느 도시에서 태어났나요?\n>>")
+#3. Ask the user for the name of a pet.
+pet = input("키우고 있는 반려 동물의 이름은 무엇인가요?\n>>")
+#4. Combine the name of their city and pet and show them their band name.
+print("당신의 밴드 이름은 🌟" + city + " " + pet + "🌟 입니다.")
+#5. Make sure the input cursor shows on a new line:
+
+# Solution: https://replit.com/@appbrewery/band-name-generator-end
