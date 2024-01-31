c31be088262a55c35b14de3fa806847d29abc4b9
Sumin Park <78036808+convin305@users.noreply.github.com>
2024-01-31 15:20:56 +0900

    1일차 : 밴드명 생성기

--- 박수민/day1.py
+++ 박수민/day1.py
@@ -1,1 +1,10 @@
-# 추후수정
+#1. Create a greeting for your program.
+print("Welcome to the Band Name Generator")
+#2. Ask the user for the city that they grew up in.
+city = input("What's name of the city you grew up in?\n")
+#3. Ask the user for the name of a pet.
+pet_name = input("What's your pet's name?\n")
+#4. Combine the name of their city and pet and show them their band name.
+print(f"Your band name could be {city} {pet_name}")
+#5. Make sure the input cursor shows on a new line
+# city와 pet_name 뒤에 \n 넣어주면 됨.
